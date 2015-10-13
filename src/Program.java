a228fa46be2bf2777a2548a650746f2a2ac6dd14
Alexc007 <Alexc007@Alexc007-PC>
2015-10-13 15:59:29 +0300

    Created first Swing(GUI) program

--- src/Program.java
+++ src/Program.java
@@ -1,18 +1,60 @@
 
 
-* @author Alexc007
- *
- */
+/** @author Alexc007
+*
+*/
+
+import javax.swing.JOptionPane;
+
+
+
+
+
+
+
 public class Program 
 {
 
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) 
 	{
-		
 
+		String firstNumber = "";
+		String secondNumber = "";
+		
+		
+		
+			
+		try 
+		{
+			firstNumber = JOptionPane.showInputDialog("Enter First Integer"); //pop out menu that ask to enter integer
+			System.out.println(firstNumber);
+			secondNumber = JOptionPane.showInputDialog("Enter Second Integer");
+			System.out.println(secondNumber);
+			
+			int number1 = Integer.parseInt(firstNumber);
+			int number2 = Integer.parseInt(secondNumber);
+			
+			int sum = number1 + number2;
+			
+			JOptionPane.showMessageDialog(null, "The sum is " + sum + " Sum of two integers");
+			
+		} catch (Exception e)
+		{
+			//JOptionPane.showMessageDialog(null, "Incorrect Values Entered");
+		JOptionPane.showMessageDialog(null, "Incorrect Values Entered"	, "Error Entry", JOptionPane.ERROR_MESSAGE);
+			System.out.println(e);
+		}
+		
+		
+	
+
+				
+		
+// STOPPED AT END OF LESSON 4
 	}
 
 }
