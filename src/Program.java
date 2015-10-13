b22a6ee8da0c945db6b7bcf524972bc400f56a65
Alexc007 <Alexc007@Alexc007-PC>
2015-10-13 16:03:45 +0300

    added comments

--- src/Program.java
+++ src/Program.java
@@ -28,25 +28,26 @@ public class Program
 		
 		
 			
-		try 
+		try //the code is inside a Try - Catch block so that if you press CANCEL or insert INCORRECT VALUE
+		// the java execution would not be interrupted by the exception - but handle it properly
 		{
 			firstNumber = JOptionPane.showInputDialog("Enter First Integer"); //pop out menu that ask to enter integer
 			System.out.println(firstNumber);
 			secondNumber = JOptionPane.showInputDialog("Enter Second Integer");
 			System.out.println(secondNumber);
 			
-			int number1 = Integer.parseInt(firstNumber);
-			int number2 = Integer.parseInt(secondNumber);
+			int number1 = Integer.parseInt(firstNumber); //convert an integer that has been inserted into a string
+			int number2 = Integer.parseInt(secondNumber); //convert an integer that has been inserted into a string
 			
 			int sum = number1 + number2;
 			
-			JOptionPane.showMessageDialog(null, "The sum is " + sum + " Sum of two integers");
+			JOptionPane.showMessageDialog(null, "The sum is " + sum + " Sum of two integers"); //open another message box that will show the sum of the numbers
 			
-		} catch (Exception e)
+		} catch (Exception e) //catch exception
 		{
 			//JOptionPane.showMessageDialog(null, "Incorrect Values Entered");
-		JOptionPane.showMessageDialog(null, "Incorrect Values Entered"	, "Error Entry", JOptionPane.ERROR_MESSAGE);
-			System.out.println(e);
+		JOptionPane.showMessageDialog(null, "Incorrect Values Entered"	, "Error Entry", JOptionPane.ERROR_MESSAGE); //display an error message box after getting an exception
+			System.out.println(e); //print out the exception itself
 		}
 		
 		
